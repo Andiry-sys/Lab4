086c186df1c999728801dfbb2ca16fa6c8a3555c
Andriy-sys <marchycandriy@gmail.com>
2022-01-10 22:57:17 +0200

    HTML and Js

---
+++ Task4.1/app.js
@@ -0,0 +1,13 @@
+let _num1 = prompt(`Enter first number `)
+let _num2 = prompt(`Enter second number `)
+
+function Minimal(a, b) {
+    if (a < b) {
+        console.log(a);
+    }
+    else {
+        console.log(b);
+    }
+}
+
+Minimal(_num1, _num2)
